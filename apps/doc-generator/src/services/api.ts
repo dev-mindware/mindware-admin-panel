0e9c17acc1df582bf2a09f6130e196765c7280e9
Johnny Cardoso <147612467+WildKnuckles@users.noreply.github.com>
2026-09-07 12:08:52 +0100

    Update api.ts

--- apps/doc-generator/src/services/api.ts
+++ apps/doc-generator/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3005/api";
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_URL,
